b4224aa5370afe77951c1e5ea3ef4d7d314a903d
latypova-alina <alinkin9696@mail.ru>
2017-08-21 02:03:55 +0300

    add rescue cases, start option and synonyms command

--- lib/bot_response.rb
+++ lib/bot_response.rb
@@ -2,9 +2,13 @@ class BotResponse
   attr_reader :bot, :message
   TB = Telegram::Bot
   TBT = TB::Types
-  MORE_INFORMATION = TBT::KeyboardButton.new(text: "/Што?")
-  KEYBOARD = TBT::ReplyKeyboardMarkup.new(keyboard: [MORE_INFORMATION], resize_keyboard: true)
+  MORE_INFORMATION = TBT::KeyboardButton.new(text: "/што?")
+  SYNONYMS = TBT::KeyboardButton.new(text: "/а синонимы?")
+  KEYBOARD = TBT::ReplyKeyboardMarkup.new(keyboard: [MORE_INFORMATION, SYNONYMS], resize_keyboard: true)
   STATHAM_FILE_ID = "CAADBAADiAIAAoVpUQWeomfO2K5XagI"
+  HELLO_FILE_ID = "CAADBAADqQIAAoVpUQV8lGGpxy7bLwI"
+  STATHAM_DOES_NOT_KNOW = "CAADBAADpwIAAoVpUQULoMsJeHJ5dQI"
+  ANGRY_STATHAM = "CAADBAADowIAAoVpUQUe179jKBFMmAI"
 
   def initialize(bot, message)
     @bot = bot
@@ -14,12 +18,34 @@ class BotResponse
 
   def give_response
     if message.text.match(/\//)
-      give_translation
+      define_command
     else
       give_definition
     end
     define_patience_counter
     @@previous_word = WordInfo.word
+  rescue
+    bot.api.sendMessage(chat_id: message.chat.id, text: "Я бы посоветовал тебе не тратить время на ерунду")
+    bot.api.sendSticker(chat_id: message.chat.id, sticker: ANGRY_STATHAM)
+  end
+
+  def define_command
+    case message.text
+    when "/што?"
+      if WordInfo.no_word?
+        bot.api.sendMessage(chat_id: message.chat.id, text: "Так не пойдет. Сначала слово")
+        bot.api.sendSticker(chat_id: message.chat.id, sticker: STATHAM_FILE_ID)
+      elsif WordInfo.no_definition? || WordInfo.no_translation?
+        bot.api.sendMessage(chat_id: message.chat.id, text: "Такого слова я не знаю")
+        bot.api.sendSticker(chat_id: message.chat.id, sticker: STATHAM_DOES_NOT_KNOW)
+      else
+        give_translation
+      end
+    when "/а синонимы?"
+      give_synonyms
+    when "/start"
+      start_message
+    end
   end
 
   def define_patience_counter
@@ -30,8 +56,13 @@ class BotResponse
     end
   end
 
+  def start_message
+    bot.api.sendMessage(chat_id: message.chat.id, text: "Всё просто - ты мне английское слово, а я тебе говорю что оно значит, дошло? Начинай")
+    bot.api.sendSticker(chat_id: message.chat.id, sticker: HELLO_FILE_ID)
+  end
+
   def give_definition
-    WordInfo.word = message.text
+    WordInfo.word = message.text.downcase
     bot.api.sendMessage(chat_id: message.chat.id, text: WordInfo.word_definition, reply_markup: KEYBOARD)
   end
 
@@ -44,4 +75,7 @@ class BotResponse
     end
   end
 
+  def give_synonyms
+    bot.api.sendMessage(chat_id: message.chat.id, text: WordInfo.word_synonyms, reply_markup: KEYBOARD)
+  end
 end
