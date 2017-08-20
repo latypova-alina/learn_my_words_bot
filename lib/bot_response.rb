3ae62de1899ea4f2bcbfd650ebfd6a23a08b3474
latypova-alina <alinkin9696@mail.ru>
2017-08-21 00:05:15 +0300

    unite bot response and command response into one class

--- lib/bot_response.rb
+++ lib/bot_response.rb
@@ -1,10 +1,10 @@
-require_relative "command_response.rb"
 class BotResponse
   attr_reader :bot, :message
   TB = Telegram::Bot
   TBT = TB::Types
   MORE_INFORMATION = TBT::KeyboardButton.new(text: "/Што?")
   KEYBOARD = TBT::ReplyKeyboardMarkup.new(keyboard: [MORE_INFORMATION], resize_keyboard: true)
+  STATHAM_FILE_ID = "CAADBAADiAIAAoVpUQWeomfO2K5XagI"
 
   def initialize(bot, message)
     @bot = bot
@@ -14,10 +14,9 @@ class BotResponse
 
   def give_response
     if message.text.match(/\//)
-      CommandResponse.new(bot, message, @@patience_counter).give_response
+      give_translation
     else
-      WordInfo.word = message.text
-      bot.api.sendMessage(chat_id: message.chat.id, text: WordInfo.word_definition, reply_markup: KEYBOARD)
+      give_definition
     end
     define_patience_counter
     @@previous_word = WordInfo.word
@@ -28,6 +27,20 @@ class BotResponse
       @@patience_counter+=1
     else
       @@patience_counter = 1
+    end
+  end
+
+  def give_definition
+    WordInfo.word = message.text
+    bot.api.sendMessage(chat_id: message.chat.id, text: WordInfo.word_definition, reply_markup: KEYBOARD)
+  end
+
+  def give_translation
+    if @@patience_counter > 1
+      bot.api.sendMessage(chat_id: message.chat.id, text: "Не 'што' а 'что', давай другое слово", reply_markup: KEYBOARD)
+      bot.api.sendSticker(chat_id: message.chat.id, sticker: STATHAM_FILE_ID)
+    else
+      bot.api.sendMessage(chat_id: message.chat.id, text: WordInfo.word_translation, reply_markup: KEYBOARD)
     end
   end
 
