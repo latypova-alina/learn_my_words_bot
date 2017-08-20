727dc105d0a5269c919d5fd8db0bb5453841de59
latypova-alina <alinkin9696@mail.ru>
2017-08-21 02:05:55 +0300

    add word_synonyms method

--- lib/word_info.rb
+++ lib/word_info.rb
@@ -10,19 +10,47 @@ class WordInfo
     def word_translation
       response = Lingvo.translation(word)
       JSON.parse(response.body)["Translation"]["Translation"]
+    rescue
+      nil
     end
 
     def word_definition
       line = ""
-      response = Mashape.definition(word)
+      response = Mashape.information(word)
       JSON.parse(response.body)["results"].each do |d|
         line << "#{d['definition'].capitalize}\n\n"
       end
       line
+    rescue
+      nil
+    end
+
+    def word_synonyms
+      line = ""
+      response = Mashape.information(word)
+      JSON.parse(response.body)["results"].each do |d|
+        line << "#{d['synonyms'].first.capitalize}\n\n" if d['synonyms']
+      end
+      line
+    rescue
+      nil
     end
 
     def the_same_word?(previous_word)
       word == previous_word
     end
+
+    def no_definition?
+      word_definition == nil
+    end
+
+    def no_translation?
+      word_translation == nil
+    end
+
+    def no_word?
+      word == nil
+    end
+
   end
 end
